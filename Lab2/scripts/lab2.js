ff1f5a1e0e3f8c3b657adb943a37c2ddea60306a
Mikolaj Ambroziak <mikolajambroziak2017@outlook.com>
2018-10-23 16:38:25 +0200

    Dodanie java scriptu który tworzy child

--- Lab2/scripts/lab2.js
+++ Lab2/scripts/lab2.js
@@ -1,1 +1,9 @@
-var p = document.createElement('p');
+var newDiv = null;
+ function function1(){
+    newDiv = document.createElement("div");
+    newDiv.setAttribute("id","klocek");
+    newDiv.style.width = "100px";
+    newDiv.style.height = "100px";
+    newDiv.style.backgroundColor = "red";
+    document.body.appendChild(newDiv);
+ }
